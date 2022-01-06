4e73cc0622b7e3eba0f1fd2d83157fbc2a69eafc
Pedro Barossi <pedro3barossi@gmail.com>
2022-01-05 21:33:29 -0300

    Update 1
    Corrigido erro no método saque e pequenas alterações sugeridas na aula. Criação do método estadoAtual

--- src/poo05/Conta.java
+++ src/poo05/Conta.java
@@ -13,6 +13,15 @@ public class Conta {
         this.status = false;
     }
     
+    public void estadoAtual(){
+        System.out.println("------------------------------");
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: R$" + this.getSaldo());
+        System.out.println("Status: " + this.isStatus());
+    }
+    
     public int getNumConta() {
         return numConta;
     }
@@ -61,48 +70,53 @@ public class Conta {
         } else if (t.equals("CP")) {
             this.setSaldo(150);
         }
+        System.out.println("Conta aberta com sucesso");
     }
     
     public void fecharConta(){
-        if (saldo > 0) {
+        if (this.getSaldo() > 0) {
             System.out.println("Conta com dinheiro");
-        } else if (saldo < 0) {
+        } else if (this.getSaldo() < 0) {
             System.out.println("Conta em débito");
         } else {
             this.setStatus(false);
+            System.out.println("Conta fechada com sucesso");
         }
     }
     
     public void depositar(float deposito){
         if (this.status = true) {
             this.setSaldo(getSaldo()+deposito);
+            System.out.println("Depósito realizado na conta de " + this.getDono());
         } else {
-            System.out.println("Impossível depositar");
+            System.out.println("Impossível depositar, conta fechada");
         }
     }
     
     public void sacar(float saque){
         if (this.status = true) {
-            if (getSaldo()>0) {
+            if (getSaldo()>=saque) {
                 this.setSaldo(getSaldo()-saque);
+                System.out.println("Saque realizado na conta de " + this.getDono());
             } else {
                 System.out.println("Saldo insuficiente");
             }
         } else {
-            System.out.println("Impossível sacar");
+            System.out.println("Impossível sacar, conta fechada");
         }
     }
     
     public void pagarMensal(){
-        float mensalidade = 0;
+        float mensal = 0;
         if (getTipo().equals("CC")) {
-            mensalidade = 12f;
+            mensal = 12f;
         } else if (getTipo().equals("CP")) {
-            mensalidade = 20f;
+            mensal = 20f;
         }
         if (this.status == true) {
-            if (this.getSaldo() > mensalidade) {
-                this.setSaldo(getSaldo()-mensalidade);
+            if (this.getSaldo() > mensal) {
+                this.setSaldo(getSaldo()-mensal);
+                System.out.println("Mensalidade paga com sucesso por " + this.getDono());
             } else {
                 System.out.println("Saldo insuficiente");
                 }
