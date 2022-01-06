f9af0c142fe3f8665b2627a61acbdf5088a1e920
Pedro Barossi <pedro3barossi@gmail.com>
2022-01-05 21:48:43 -0300

    Update 2
    Testes realizados na classe principal, aula concluída com sucesso

--- src/poo05/POO05.java
+++ src/poo05/POO05.java
@@ -4,7 +4,22 @@ public class POO05 {
 
 
     public static void main(String[] args) {
+       Conta p1 = new Conta();
+       p1.setNumConta(1111);
+       p1.setDono("Jubileu");
+       p1.abrirConta("CC");
        
+       Conta p2 = new Conta();
+       p2.setNumConta(2222);
+       p2.setDono("Creuza");
+       p2.abrirConta("CP");
+       
+       p1.depositar(100);
+       p2.depositar(500);
+       p2.sacar(100);
+       
+       p1.estadoAtual();
+       p2.estadoAtual();
     }
     
 }
